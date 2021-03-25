440817bbd299d1a0f1da610e6a7f8c9efde3b341
Emiline Doerr <edoerr@unomaha.edu>
2021-03-24 21:21:18 -0500

    Update heading

--- webcontent/viraldrugDB.php
+++ webcontent/viraldrugDB.php
@@ -1,12 +1,10 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
   <head>
-    <title>Viral Drug Database Front End</title>
+    <meta charset="UTF-8">
+    <title>ViralDrugDBe</title>
   </head>
-
 <body>
-  <?php
-    echo "<p>Hello World</p>";
-  ?>
+  <h1><?php echo "<p>Database for a Small Set of Viruses and Associated Antiviral Drugs</p>"; ?></h1>
 </body>
-
 </html>
