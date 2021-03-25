4d6c50f260941c6859d03a764588fabf55627925
Emiline Doerr <edoerr@unomaha.edu>
2021-03-24 20:26:04 -0500

    Write script to publish

--- src/scripts/publish.sh
+++ src/scripts/publish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Name:  Emiline Doerr
 # Class: BIOI 4870, Spring 2021
 # Course Project
@@ -15,17 +16,31 @@
 #
 # Publish front end code in project to public_html folder
 # in Odin.
+#
+# Resources:
+# - Error handling
+# linuxcommand.org/lc3_wss0140.php
+# 
+# Script follows here:
 
-# Placeholder
-echo('Hello')
+# 1. Read filename
+echo "Name of file in webcontent/ to publish:"
+read FILENAME
+# 2. Check if file exists (Error handling)
+if ls ~/viraldrugDB/webcontent/$FILENAME; then
+	# TODO: check if file contents match
+	echo "Publishing file $FILENAME..."
+	
+	# 3. Remove file from public_html
+	rm ~/public_html/$FILENAME
 
+	# 4. Copy file from project to public_html
+	cp ~/viraldrugDB/webcontent/$FILENAME ~/public_html/
 
-# 1. Read filename or publish all
-# 2. See if ~/public_html/(file) exists/matches contents of (file)
-# 3. If yes: remove file from public_html 
-# rm ~/public_html/(file)
-# 4. Copy file to public_html folder 
-# cp ~/viraldrugDB/webcontent/(file) ~/public_html/(file)
-# 5. Update user permissions to publish
-# chmod +x ~/public_html/(file)
-
+	# 5. Update permissions to publish
+	chmod +x ~/public_html/$FILENAME
+	echo "File $FILENAME successfully published."
+else
+	echo "File $FILENAME does not exist. Aborting." 1>&2
+	exit 1
+fi
