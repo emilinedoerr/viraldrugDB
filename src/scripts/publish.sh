26133a9c93f721e9846c45cf190832dd1b1154e4
Emiline Doerr <edoerr@unomaha.edu>
2021-03-24 19:22:03 -0500

    Add directory for scripts, create script for publishing webpages

---
+++ src/scripts/publish.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Name:  Emiline Doerr
+# Class: BIOI 4870, Spring 2021
+# Course Project
+#
+# Created: March 24
+#
+# Honor Pledge: On my honor as a student of the University of Nebraska at
+# Omaha, I have neither given nor received unauthorized help on
+# this programming assignment.
+#
+# NAME: Emiline Doerr
+# NUID: 628
+# EMAIL: edoerr@unomaha.edu
+#
+# Publish front end code in project to public_html folder
+# in Odin.
+
+# Placeholder
+echo('Hello')
+
+
+# 1. Read filename or publish all
+# 2. See if ~/public_html/(file) exists/matches contents of (file)
+# 3. If yes: remove file from public_html 
+# rm ~/public_html/(file)
+# 4. Copy file to public_html folder 
+# cp ~/viraldrugDB/webcontent/(file) ~/public_html/(file)
+# 5. Update user permissions to publish
+# chmod +x ~/public_html/(file)
+
