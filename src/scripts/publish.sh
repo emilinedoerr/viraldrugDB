386c6059fb677ce85b02d4cc7874801307e74215
Emiline Doerr <edoerr@unomaha.edu>
2021-03-24 21:05:01 -0500

    Add variables

--- src/scripts/publish.sh
+++ src/scripts/publish.sh
@@ -18,8 +18,12 @@
 # in Odin.
 #
 # Resources:
+# - Variables
+# tutorialspoint.com/unix/unix-using-variables.htm
 # - Error handling
 # linuxcommand.org/lc3_wss0140.php
+# - File comparison
+# baeldung.com/linux/fastest-check-files-same-content
 # 
 # Script follows here:
 
@@ -34,14 +38,21 @@ error_exit()
 # 1. Read filename
 echo "Name of file in webcontent/ to publish:"
 read FILENAME
+FILEPATH=~/viraldrugDB/webcontent/$FILENAME
+PUBLIC_HTML=~/public_html/
 # 2. Check if file exists (Error handling)
-[ -e ~/viraldrugDB/webcontent/$FILENAME ] || error_exit "File ~/viraldrugDB/webcontent/$FILENAME does not exist. Aborting." 
-# TODO: check if file contents match
-echo "Publishing file $FILENAME..."	
-# 3. Remove file from public_html
-rm ~/public_html/$FILENAME
+[ -e $FILEPATH ] || error_exit "File $FILEPATH does not exist. Aborting." 
+# 3. Check if file exists in public_html/
+if [ -e $PUBLIC_HTML$FILENAME ]; then
+	# 3.1. Remove file from public_html
+	rm $PUBLIC_HTML$FILENAME
+fi
+# (TODO) 4. Check if file contents match
+#if cmp -e  /
+
+echo "Publishing file $FILENAME to public_html/..."	
 # 4. Copy file from project to public_html
-cp ~/viraldrugDB/webcontent/$FILENAME ~/public_html/
+cp $FILEPATH $PUBLIC_HTML 
 # 5. Update permissions to publish
-chmod +x ~/public_html/$FILENAME
+chmod +x $PUBLIC_HTML$FILENAME
 echo "File $FILENAME successfully published."
