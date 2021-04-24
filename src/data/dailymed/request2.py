e377392df77bffdcdc7c57a40a17cf5094655d4d
Emiline Doerr <edoerr@unomaha.edu>
2021-04-24 15:54:29 -0500

    start implementing ability to make different requests

--- src/data/dailymed/request2.py
+++ src/data/dailymed/request2.py
@@ -1,8 +1,24 @@
+import sys
+import getopt
 import json
 import requests
 
+
 def main():
-    
+   
+    # Initialize parameters
+    service = ""
+
+    # Read in the command-line arguments into the opts list.
+    # Opts: -s [SERVICE] 
+    opts, args = getopt.getopt(sys.argv[1:], "s:")
+
+    # Process the opt and arg lists displaying the argument of
+    # each option.
+    for (opt, arg) in opts:
+        # Specifies service
+        if opt == '-s':
+            print(str(arg))
 
     base = "https://dailymed.nlm.nih.gov/dailymed/services/"
     filters = "?drug_name=acyclovir"
@@ -13,6 +29,8 @@ def main():
     req_ndcs = base + ndc + file_ext + filters
     req_names = base + names + file_ext + filters
     #request = base + resource + file_ext + filters 
+    
+    request = req_ndcs
     print(request)
     response = requests.get(request)
     drugnames = json.loads(response.text)
