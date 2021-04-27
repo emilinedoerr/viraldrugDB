0bef14d2ff1cf8216b8ed2277f41aaea5c614a48
Emiline Doerr <edoerr@unomaha.edu>
2021-04-27 00:13:27 -0500

    script to pull data from dailymed api

---
+++ src/scripts/pull_dailymed.py
@@ -0,0 +1,64 @@
+import requests
+import json
+
+# pull_dailymed.py
+
+# @author Emiline Doerr
+
+# Pull all data from dailymed given a list of viruses and drugs
+# runs with Python3 
+
+# Global variables:
+
+# URL prefix for every query
+url = "https://dailymed.nlm.nih.gov/dailymed/services/v2/"
+
+# Return file extension (always json)
+ext = ".json"
+
+# Viruses
+virus_list = ['herpes', 'hepatitis', 'influenza']
+
+# Files with drugs
+druglist_path = "/home/edoerr/viraldrugDB/src/data/"
+
+
+
+def main():
+
+
+    # For each virus:
+    # (for now only process one at a time)
+    virus = virus_list[0]
+    
+    # Read in file with drug list
+    filepath = druglist_path + virus + "_drugs.txt"
+    drug_file = open(filepath, 'r')
+    drug_list = list()
+    line = drug_file.readline()
+    while (line):
+        drug_list += [line.strip()]
+        line = drug_file.readline()
+    drug_file.close()
+    
+    for drug in drug_list:
+        # query dailymed spls for drug name
+        query = url + "spls" + ext + "?drug_name=" + drug
+        print(query)
+        
+    # request
+    response = requests.get(query)
+    data = json.loads(response.text)['data']
+    
+    # extract set ids
+    setid_list = [i['setid'] for i in data] 
+    print(setid_list)
+
+    # keep going if time ...
+    # for each set id:
+    # extract ndc list
+    # return total ndc list to drug
+
+
+if __name__ == '__main__':
+    main()
