01db3c0b388a7bae121edd017382fa25b2868a49
Emiline Doerr <edoerr@unomaha.edu>
2021-04-27 18:22:26 -0500

    complete file paths

--- src/scripts/update.sh
+++ src/scripts/update.sh
@@ -5,18 +5,19 @@
 
 # Variables
 MYSQL_DB="edoerr"
+FILEPATH="/home/edoerr/viraldrugDB/src"
 
 # run python script, replace update
-python3 /home/edoerr/viraldrugDB/src/scripts/pull_kegg.py > update.kegg.sql
-python3 /home/edoerr/viraldrugDB/src/scripts/pull_pubchem.py > update.pubchem.sql
-python3 /home/edoerr/viraldrugDB/src/scripts/pull_dailymed.py > update.dailymed.sql
+python3 $FILEPATH/scripts/pull_kegg.py > $FILEPATH/data/update.kegg.sql
+python3 $FILEPATH/scripts/pull_pubchem.py > $FILEPATH/data/update.pubchem.sql
+python3 $FILEPATH/scripts/pull_dailymed.py > $FILEPATH/data/update.dailymed.sql
 
 # update table in mysql
-mysql $MYSQL_DB < update.kegg.sql
-mysql $MYSQL_DB < update.pubchem.sql
-mysql $MYSQL_DB < update.dailymed.sql
+mysql $MYSQL_DB < $FILEPATH/data/update.kegg.sql
+mysql $MYSQL_DB < $FILEPATH/data/update.pubchem.sql
+mysql $MYSQL_DB < $FILEPATH/data/update.dailymed.sql
 
 # remove files
-rm update.kegg.sql
-rm update.pubchem.sql
-rm update.dailymed.sql
+rm $FILEPATH/data/update.kegg.sql
+rm $FILEPATH/data/update.pubchem.sql
+rm $FILEPATH/data/update.dailymed.sql
