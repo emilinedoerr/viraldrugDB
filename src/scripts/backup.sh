466369df130eab746b08a14fa67b86aaca7728cb
Emiline Doerr <edoerr@unomaha.edu>
2021-04-27 18:23:56 -0500

    add comment

--- src/scripts/backup.sh
+++ src/scripts/backup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Backup data in viraldrugDB via mysqldump.
+
+
 mysqldump edoerr virus > /home/edoerr/viraldrugDB/src/data/virus.dump.sql
 mysqldump edoerr drug > /home/edoerr/viraldrugDB/src/data/drug.dump.sql
 
